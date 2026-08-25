647a71f2875c57d60630d09e8ac8099aeff3ebd3
iv_androo <evandroa174@gmail.com>
2026-08-25 14:48:24 +0000

    Estrutura do cabeçalho, adição de imagens e definição de variaveis de cor

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex justify-between items-center py-4 px-12">
+        <div>
+          <img
+            src="./logo.jpeg"
+            alt="Seatrail logo"
+            className="w-14 rounded-full"
+          />
+        </div>
+        <nav>
+          <ul className="inline-flex bg-neve py-2 px-6 rounded-full items-center gap-4 font-medium lowercase text-secundaria">
+            <li className="flex items-center bg-white px-2 rounded-full ">
+              <Link href="">Casa</Link>
+            </li>
+            <li className="flex items-center px-2 rounded-full transition-all duration-150 ease-in hover:bg-white ">
+              <Link href="">Quem somos</Link>
+            </li>
+            <li className="flex items-center px-2 rounded-full transition-all duration-150 ease-in hover:bg-white ">
+              <Link href="">MVV</Link>
+            </li>
+            <li className="flex items-center px-2 rounded-full transition-all duration-150 ease-in hover:bg-white ">
+              <Link href="">Serviços</Link>
+            </li>
+            <li className="flex items-center px-2 rounded-full transition-all duration-150 ease-in hover:bg-white ">
+              <Link href="">Sectores</Link>
+            </li>
+            <li className="flex items-center px-2 rounded-full transition-all duration-150 ease-in hover:bg-white ">
+              <Link href="">Contacto</Link>
+            </li>
+          </ul>
+        </nav>
+          <button className="flex items-center px-6 py-2 rounded-full bg-destaque text-base font-medium text-secundaria cursor-pointer">
+            Contactar
+          </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
